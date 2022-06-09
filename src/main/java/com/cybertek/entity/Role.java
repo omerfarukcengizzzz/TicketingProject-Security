957a0a7811bf83c9c5c8f1e5dc556e59a6caef82
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-09 16:13:58 +0100

    Ticketing Project - Security

--- src/main/java/com/cybertek/entity/Role.java
+++ src/main/java/com/cybertek/entity/Role.java
@@ -6,11 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,8 +17,5 @@ import java.util.List;
 public class Role extends BaseEntity {
 
     private String description;
-
-    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
-    private List<User> users = new ArrayList<>();
 
 }
