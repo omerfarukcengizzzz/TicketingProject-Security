32504adcd5a72378ff02e61997a94b8a228b0b5e
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-09 12:50:08 +0100

    Ticketing Project - Security

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String login(){
-        return "login";
+        return "/login";
     }
 
     @RequestMapping("/welcome")
     public String welcome(){
-        return "welcome";
+        return "/welcome";
     }
 
 }
