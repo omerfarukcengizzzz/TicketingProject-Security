ee5b334dc5795537563082f59c38ff68656102b8
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-24 06:58:06 +0100

    Ticketing Project - Security

--- src/main/java/com/cybertek/entity/BaseEntityListener.java
+++ src/main/java/com/cybertek/entity/BaseEntityListener.java
@@ -1,4 +1,46 @@
 package com.cybertek.entity;
 
-public class BaseEntityListener {
+import com.cybertek.entity.common.UserPrincipal;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@Component
+public class BaseEntityListener extends AuditingEntityListener {
+
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    @PrePersist
+    public void prePersist(BaseEntity baseEntity) {
+
+        baseEntity.setInsertDateTime(LocalDateTime.now());
+        baseEntity.setLastUpdateDateTime(LocalDateTime.now());
+        baseEntity.setInsertUserId(1L);
+        baseEntity.setLastUpdateUserId(1L);
+
+        if (authentication != null && !authentication.getName().equals("anonymousUser")) {
+            Object principal = authentication.getPrincipal();
+            baseEntity.setInsertUserId(((UserPrincipal) principal).getId());
+            baseEntity.setLastUpdateUserId(((UserPrincipal) principal).getId());
+        }
+
+    }
+
+    @PreUpdate
+    public void preUpdate(BaseEntity baseEntity) {
+
+        baseEntity.setLastUpdateDateTime(LocalDateTime.now());
+        baseEntity.setLastUpdateUserId(1L);
+
+        if (authentication != null && !authentication.getName().equals("anonymousUser")) {
+            Object principal = authentication.getPrincipal();
+            baseEntity.setLastUpdateUserId(((UserPrincipal) principal).getId());
+        }
+
+    }
 }
