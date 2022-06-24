f93553dafb3c2a91c7bd5ae615986b3a3a7a8077
Omer Faruk CENGIZ <omerfarukcengizzzz@gmail.com>
2022-06-24 06:58:02 +0100

    Ticketing Project - Security

--- src/main/java/com/cybertek/entity/BaseEntity.java
+++ src/main/java/com/cybertek/entity/BaseEntity.java
@@ -31,19 +31,4 @@ public class BaseEntity {
 
     private Boolean isDeleted = false;
 
-    @PrePersist
-    private void onPrePersist() {
-        this.insertDateTime = LocalDateTime.now();
-        this.lastUpdateDateTime = LocalDateTime.now();
-        // will be done with security
-        this.insertUserId = 1L;
-        this.lastUpdateUserId = 1L;
-    }
-
-    @PreUpdate
-    private void onPreUpdate() {
-        this.lastUpdateDateTime = LocalDateTime.now();
-        this.lastUpdateUserId = 1L;
-    }
-
 }
